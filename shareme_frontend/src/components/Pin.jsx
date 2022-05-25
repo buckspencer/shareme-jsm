d5e0f3f99030977aff56bf7c57e843e8adcdb68e
Buck Spencer <buck.spencer@mutations.ltd>
2022-05-25 13:40:33 -0700

    Remose user handle from feed

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -123,14 +123,7 @@ const Pin = ({ pin }) => {
       <Link
         to={`/user-profile/${postedBy?._id}`}
         className="flex gap-2 mt-2 items-center"
-      >
-        <img
-          className="w-8 h-8 rounded-full object-cover"
-          src={postedBy?.image}
-          alt="user-profile"
-        />
-        <p className="font-semibold capitalize">{postedBy?.userName}</p>
-      </Link>
+      ></Link>
     </div>
   );
 };
